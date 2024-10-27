f1a1f58fea6ca8ffa926f0f795ea86a586579961
NTThuy26 GitHub <23110336@hcmute.edu.vn>
2024-10-27 18:27:46 +0700

    data_visualization[update]

--- modules/data_visualization.py
+++ modules/data_visualization.py
@@ -2,33 +2,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 print("TRỰC QUAN HÓA DATA".center(50, '='))
-def plot_average_scores(df):
-    """Vẽ biểu đồ điểm trung bình các môn."""
-    subjects = ['toan', 'ngu_van', 'ngoai_ngu', 'vat_li', 'hoa_hoc', 'sinh_hoc',
-                'lich_su', 'dia_li', 'gdcd']
-    averages = df[subjects].mean()
+def plot_grade(df):
+    """Vẽ biểu đồ điểm học tập."""
+    grade_cols = ['english.grade','math.grade','sciences.grade','language.grade']
+
+    df_grades = df[grade_cols]
 
     # Tạo màu thay đổi cho từng cột (tạo dãy màu)
-    colours = plt.cm.viridis(np.linspace(0, 1, len(subjects)))
+    colours = plt.cm.viridis(np.linspace(0, 1, len(grade_cols)))
 
-    plt.bar(subjects, averages, color = colours)
+    df_grades.plot(kind='bar', figsize=(10, 6))
 
-    for i, value in enumerate(averages):
-        plt.text(i, value + 0.05, round(value, 2), ha='center', va='bottom')
+    plt.title("Comparison of Grades in Different Subjects")
+    plt.xlabel("Student ID")
+    plt.ylabel("Grades")
 
-    plt.xlabel('Môn học')
-    plt.ylabel('Điểm trung bình')
-    plt.title('Điểm trung bình các môn thi THPT 2024')
-
-    # in nghiêng các subject
-    plt.xticks(rotation=45)
-
-    # tạo lưới mờ
+    # Tạo lưới mờ
     plt.grid(axis='y', linestyle='--', alpha=0.5)
 
-    plt.tight_layout()
+    # Thêm số liệu trên đầu các cột
+    for i, value in enumerate(df_grades):
+        plt.text(i, value + 0.05, round(value, 2), ha='center', va='bottom')
+
+    # In nghiêng các subject
+    plt.xticks(rotation=45)
+
+    # Lấy tên của các cột làm nhãn cho chú thích
+    plt.legend(title="Subjects")
 
     plt.show()
-
+    '''
+    # Tạo heatmap 
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(df[ratings_cols].T, annot=True, cmap="YlGnBu", cbar=True, linewidths=0.5)
+    plt.title("Heatmap of Grades and Portfolio Ratings")
+    plt.xlabel("Student ID")
+    plt.ylabel("Grades / Ratings")
     plt.show()
-
+    '''
